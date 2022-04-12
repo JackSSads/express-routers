26ba93f5ef01a484f19204805d1edc51a38e3492
Jackson Souza <67111463+JackSSads@users.noreply.github.com>
2022-04-11 21:53:37 -0300

    Update index.js

--- users/index.js
+++ users/index.js
@@ -1,32 +1,29 @@
-const express = require('express')
-const router = express.Router()
-const path = require("path")
+const express = require('express');
+const router = express.Router();
+const path = require("path");
 
 
-const basePath = path.join(__dirname, '../templates') // __dirname referencia o diretório atual
+const basePath = path.join(__dirname, '../templates');
 
 router.get('/add', (req, res) => {
-    res.sendFile(`${basePath}/userform.html`)
-})
+    res.sendFile(`${basePath}/userform.html`);
+});
 
 router.post('/save', (req, res) => {
-    console.log(req.body)
+    const name =  req.body.name;
+    const age =  req.body.age;
 
-    const name =  req.body.name
-    const age =  req.body.age
+    console.log(`O nome do usuário é ${name} e ele tem ${age} anos`);
 
-    console.log(`O nome do usuário é ${name} e ele tem ${age} anos`)
-
-    res.sendFile(`${basePath}/userform.html`)
-})
+    res.sendFile(`${basePath}/userform.html`);
+});
 
 router.get('/:id', (req, res) => {
-    const id = req.params.id
+    const id = req.params.id;
 
-    //leitura de tabelas users, resgatar um usuári do banco
-    console.log(`Estamos buscando pelo usuário: ${id}`)
+    console.log(`Estamos buscando pelo usuário: ${id}`);
 
-    res.sendFile(`${basePath}/users.html`)
-})
+    res.sendFile(`${basePath}/users.html`);
+});
 
-module.exports = router
+module.exports = router;
